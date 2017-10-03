6b7c0f28fa9375a3b8af9e05bc41f8b6d4519f1a
Colombianito66 <colombianito66@gmail.com>
2017-10-03 09:36:37 -0700

    complete first-C language practice

--- first-c.C
+++ first-c.C
@@ -1,3 +1,38 @@
-int main(int argc. char** argv) {
+// Every C program is an operating system process/executable
+// Specified the same kind of comments
+
+// #includes allow us to combine different files into a single program
+// #including can be pointed at a file that you wrote, or ...
+
+#include <stdio.h>
+
+// int integerFunction(int input) {
+//   return 0;
+// }
+// float floatFunction(float input) {
+//   return 0.0;
+// }
+// float mixedFunction(float input) {
+//   return 0.0;
+// }
+
+int main(int argc, char** argv) {
+  //return 0;
+  // argv = [
+  //   ['g', 'c', 'c'],
+  //   ['a', 'b', 'c'],
+  //   ['g', 'c', 'c'],
+  //   ['h', 'i']
+  // ]
+  // a string is in double quotes
+  // a character (char) is in single quotes
+  return 100;
+
+
+  // printf("argc is: ");
+  // printf("%d\n", argc);
   
+  // for(int i = 0; i < argc; ++i) {
+  //   printf("%s\n", argv[i]);
+  // }
 }
