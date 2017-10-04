77087cbe9d31c57cfd91dbab7c9f72932a08fb63
JCamp-12 <username@users.noreply.github.com>
2017-10-04 16:14:26 -0400

    finishing up

---
+++ Test for C/cProgram.C
@@ -0,0 +1,21 @@
+// Every  program is an operating system process/executable
+
+/*
+*  Multi line comments
+*/
+
+int main(int argc, char** argv) {
+  // char** means one of two things: 1. someone is doing a omplicated algorithm with this object
+  // or 2. it means it is a two dimensional array like this char[][]
+  //
+  // argv = [
+  //   ['g','c','c'],
+  //   ['c','f','i','l','e'],
+  //   ['H','i'}]
+  // ]
+  // In C, a string is "an array of characters"
+  // A string is in "double quotes"
+  // A character (char) is in 'single quotes'
+
+  return 0;
+}
---
+++ Test for C/test1.C
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MYNAME "Jason Campbell"
+
+int globalVar = 100;
+
+int main(int argc, char *argv[]){
+
+    char firstLetter = 'J';
+    int age = 34;
+    long int superBigNum = -327670000;
+    float piValue = 3.1459;
+    double reallyBigPi = 3.1459145914591459;
+    printf("\n");
+
+    printf("This will print to screen \n\n");
+
+    printf("Hello world. \n\n");
+
+    printf("I am %d years old\n\n", age);
+
+    printf("Big Number %ld\n\n", superBigNum);
+
+    printf("Pi = %.5f\n\n", piValue);
+
+    printf("Big Pi = %15f\n\n", reallyBigPi);
+
+
+ return 0;
+}
+
+// int main(int argc, char *argv[]) {
+//  printf("Hello world. \n\n");
+
