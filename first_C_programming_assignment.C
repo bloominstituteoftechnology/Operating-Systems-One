401f977cc334918c20632f2551e2b9040a38cc95
Dan Winslow <dwinslow123@me.com>
2017-10-04 13:59:17 -0700

    I think I have everything but the double

--- first_C_programming_assignment.C
+++ first_C_programming_assignment.C
@@ -41,8 +41,35 @@
 // ./my_output
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
-  printf("Hello world!");
+  printf("Hello world!\n");
+  printf("%d \n", 32); // integer
+  printf("%ld \n", 312322323123); // large integer
+  printf("%f \n", 32.2); // float
+  printf("%lf \n", 3234234.22); // large float
+  printf("%c \n", 'a'); // character (char)
+  printf("%s \n", "Hello World!"); // string
+
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", i);
+  }
+
+  struct Person {
+    char name[100];
+    char height[100];
+    char favoriteColor[100];
+  };
+
+  struct Person FirstPerson;
+
+  strcpy( FirstPerson.name, "Thomson");
+  strcpy( FirstPerson.height, "7ft, 13in");
+  strcpy( FirstPerson.favoriteColor, "clear");
+
+  printf("Hi, my name is: %s\n", FirstPerson.name);
+  printf("I am: %s tall\n", FirstPerson.height);
+  printf("%s is my favorite color\n", FirstPerson.favoriteColor);
 }
 
